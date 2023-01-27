5dd747d71de970c8ca77cc314b60a5cf8dca0f71
cherry-aisha <113786104+cherry-aisha@users.noreply.github.com>
2023-01-27 22:21:51 +0000

    Added updateTimeSlot function

--- script.js
+++ script.js
@@ -29,13 +29,33 @@ function createTimeSlot() {
 }
 
 function onSaveTask(e) {
-    var hour = $(e.target).parent().parent().attr('data-hour');
+    var hour = $(e.target).parent().parent().attr("data-hour");
     var task = $(e.target).parent().prev().children().val();
 
     localStorage.setItem(hour, task);
 
     console.log("saved")
 }
+
+function updateTimeSlot () {
+    var currentHour = moment().hour();
+
+    $(".time-block").each(function (index, element) {
+
+        var hour = $(element).attr("data-hour");
+
+        if(hour < currentHour) {
+            $(element).find(".description").addClass("past");
+        }
+        else if (hour == currentHour) {
+            $(element).find("description").addClass("present");
+        }
+        else {
+            $(element).find("description").addClass("future");
+        }
+
+        });
+    }
 
 
 /*     <div class="row">
